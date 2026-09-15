6a1824624afce358b873d90faa83fa039993f11e
BEYTYC <beytyc@gmail.com>
2026-09-15 14:06:34 -0500

    Create msalConfig.js

---
+++ auth/msalConfig.js
@@ -0,0 +1,27 @@
+export const msalConfig = {
+  auth: {
+    clientId: "be68b5b7-e7eb-45e2-98f4-e5ffd8888ce6",
+    authority: "https://login.microsoftonline.com/common",
+    redirectUri: window.location.origin,
+    postLogoutRedirectUri: window.location.origin,
+    navigateToLoginRequestUrl: true,
+  },
+  cache: {
+    cacheLocation: "sessionStorage",
+    storeAuthStateInCookie: false,
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: () => {},
+      logLevel: "Error",
+    },
+  },
+};
+
+export const loginRequest = {
+  scopes: ["User.Read", "Sites.Read.All"],
+};
+
+export const graphConfig = {
+  graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
+};
