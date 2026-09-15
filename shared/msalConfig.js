d5c13bcb4865c1e29231cf11584ad4f59d87b44b
BEYTYC <beytyc@gmail.com>
2026-09-15 15:49:56 -0500

    Update msalConfig.js

--- shared/msalConfig.js
+++ shared/msalConfig.js
@@ -4,8 +4,8 @@
 
 export const msalConfig = {
   auth: {
-    clientId: "be68b5b7-e7eb-45e2-98f4-e5ffd8888ce6",
-    // Apunta directo al tenant de la ENAP en vez del endpoint genérico "common"
+    clientId: "d0326150-ca40-47f0-87cb-201c9fc721cd",
+    // Apunta directo al tenant de la ENAP
     authority: "https://login.microsoftonline.com/f53f66b3-ea23-461a-b6ff-01654042a799",
     redirectUri: window.location.origin + "/",
   },
